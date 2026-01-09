af63cd314668ab688266b2d23cdc1a9cf61a94a0
Lucian Cioba <luciancioba@gmail.com>
2026-01-09 13:08:34 +0200

    Tragic discovery of handwashing main py

---
+++ main.py
@@ -0,0 +1,229 @@
+import pandas as pd
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import plotly.express as px
+import numpy as np
+import seaborn as sns
+
+from matplotlib.lines import lineStyles
+import scipy.stats as stats
+
+# Notebook Presentation
+pd.options.display.float_format = '{:,.2f'.format
+
+# Create locators for ticks on the time axis
+years = mdates.YearLocator()
+months = mdates.MonthLocator()
+years_fmt = mdates.DateFormatter('%Y')
+
+# Register date converters to avoid warning messages
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+# Read the Data
+df_yearly = pd.read_csv('annual_deaths_by_clinic.csv')
+df_monthly = pd.read_csv('monthly_deaths.csv',
+                         parse_dates=['date'])
+
+# Preliminary Data Exploration
+print(df_yearly.shape)
+df_yearly
+
+print(df_monthly.shape)
+df_monthly
+
+# Check for Nan and Duplicates
+df_yearly.info()
+df_monthly.info()
+
+
+# Alternative to using .info()
+print(f'Any yearly NaN values? {df_monthly.isna().values.any()}')
+print(f'Any monthly NaN values? {df_yearly.isna().values.any()}')
+
+print(f'Any yearly duplicates? {df_yearly.duplicated().values.any()}')
+print(f'Any monthly duplicates? {df_monthly.duplicated().values.any()}')
+
+# Descriptive Statistics
+df_yearly.describe()
+df_monthly.describe()
+
+# Percentage pf Women Dying in childbirth
+prob = df_yearly.deaths.sum() / df_yearly.births.sum() * 100
+print(f'Chances of dying in the 1840s in Vienna: {prob:.3}%')
+
+
+# Visualise the Total Number of Births and Deaths over Time
+
+# Plot the Monthly data on twin axes
+plt.figure(figsize=(14,8), dpi=200)
+plt.title('Total Number of Monthly Births and Deaths', fontsize=18)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+
+ax1.grid(color='grey', linestyle='--')
+
+ax1.plot(df_monthly.date,
+         df_monthly.births,
+         color='skyblue',
+         linewidth=3)
+
+ax2.plot(df_monthly.date,
+         df_monthly.births,
+         color='skyblue',
+         linewidth=3)
+
+ax2.plot(df_monthly.date,
+         df_monthly.births,
+         color='crimson',
+         linewidth=2,
+         linestyle='--')
+plt.show()
+
+
+# The Yearly Data Split by Clinic
+line = px.line(df_yearly,
+               x='year',
+               y='births',
+               color='clinic',
+               title='Total Yearly Births by Clinic')
+
+line.show()
+
+line = px.line(df_yearly,
+               x='year',
+               y='deaths',
+               color='clinic',
+               title='Total Yearly Deaths by Clinic')
+
+line.show()
+
+
+# The Effect of Handwashing
+
+# Date of mandatory handwashing
+handwashing_start = pd.to_datetime('1847-06-01')
+
+df_monthly['pct_deaths'] = df_monthly.deaths/df_monthly.births
+# Split monthly into before and after handwashing_start
+before_washing = df_monthly[df_monthly.date < handwashing_start]
+after_washing = df_monthly[df_monthly.date >= handwashing_start]
+
+bw_rate = before_washing.deaths.sum() / before_washing.births.sum() * 100
+aw_rate = after_washing.deaths.sum() / after_washing.births.sum() * 100
+print(f'Average death rate before 1847 was {bw_rate:.4}%')
+print(f'Average death rate after 1847 was {aw_rate:.3}%')
+
+# Calculate a Rolling Average of the Death Rate
+# Convert date column to index first so does not get dropped
+roll_df = before_washing.set_index('date')
+roll_df = roll_df.rolling(window=6).mean()
+roll_df
+
+# Highlighting Subsections of a Line Chart
+plt.figure(figsize=(14,8), dpi=200)
+plt.title('Percentage of Monthly Deaths over Time', fontsize=18)
+plt.yticks(fontsize=14)
+plt.xticks(fontsize=14, rotation=45)
+
+plt.ylabel('Percentage of Deaths', color='crimson', fontsize=18)
+
+ax = plt.gca()
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.xaxis.set_minor_locator(months)
+ax.set_xlim([df_monthly.date.min(), df_monthly.date.max()])
+
+plt.grid(color='grey', linestyle='--')
+
+ma_line, = plt.plot(roll_df.index,
+                   roll_df.pct_deaths,
+                    color='crimson',
+                    linewidth=3,
+                    linestyle='--',
+                    label='6m Moving Average')
+bw_line, = plt.plot(before_washing.date,
+                    before_washing.pct_deaths,
+                    color='black',
+                    linewidth=1,
+                    linestyle='--',
+                    label='Before Handwashing')
+aw_line, = plt.plot(after_washing.date,
+                    after_washing.pct_deaths,
+                    color='skyblue',
+                    linewidth=3,
+                    marker='o',
+                    label='After Handwashing')
+
+plt.legend(handles=[ma_line, bw_line, aw_line],
+           fontsize=18)
+plt.show()
+
+
+# Statistics - Calculate the Difference in the average monthly death rate
+avg_prob_before = before_washing.pct_deaths.mean() * 100
+print(f'Chance of death during childbirth before handwashing: {avg_prob_before:.3}%.')
+
+avg_prob_after = after_washing.pct_deaths.mean() * 100
+print(f'Chance of death during childbirth AFTER handwashing: {avg_prob_after:.3}%.')
+
+mean_diff = avg_prob_before - avg_prob_after
+print(f'Handwashing reduced the monthly proportion of deaths by {mean_diff:.3}%!')
+
+times = avg_prob_before / avg_prob_after
+print(f'This is a {times:.2}x improvement!')
+
+# Use Box Plots to show the death rate changed before and after handwashing
+# NumPy .where() method
+df_monthly['washing_hands'] = np.where(df_monthly.date < handwashing_start, 'No', 'Yes')
+box = px.box(df_monthly,
+             x='washing_hands',
+             y='pct_deaths',
+             color='washing_hands',
+             title='How Have the Stats Changed with Handwashing?')
+
+box.update_layout(xaxis_title='Washing Hands?',
+                  yaxis_title='Percentage of Monthly Deaths',)
+
+box.show()
+
+
+# Histograms to visualise the monthly distribution of outcomes
+hist = px.histogram(df_monthly,
+                    x='pct_deaths',
+                    color='washing_hands',
+                    nbins=30,
+                    opacity=0.6,
+                    barmode='overlay',
+                    histnorm='percent',
+                    marginal='box')
+
+hist.update_layout(xaxis_title='Proportion of Monthly Deaths',
+                   yaxis_title='Count')
+hist.show()
+
+# # Kernel Density Estimate (KDE) to visualise smooth distribution
+# plt.figure(dpi=200)
+# # By default, the distribution estimate includes a negative death rate!
+# sns.kdeplot(before_washing.pct_deaths, shade=True)
+# sns_kdeplot(after_washing.pct_deaths, shade=True)
+# plt.title('Est. Distribution of Monthly Death Rate Before and After Hand-washing')
+# plt.show()
+#
+# plt.figure(dpi=200)
+# sns.kdeplot(before_washing.pct_deaths,
+#             shade=True,
+#             clip=(0,1))
+# sns_kdeplot(after_washing.pct_deaths,
+#             shade=True,
+#             clip(0,1))
+# plt.title('Est. Distribution of montlhy deaths rate before and after handwashing')
+# plt.xlim(0, 0.40)
+# plt.show()
+
+t_stat, p_value = stats.ttest_ind(a=before_washing.pct_deaths,
+                                  b=after_washing.pct_deaths)
+print(f'p-palue is {p_value:.10f}')
+print(f't-statstic is {t_stat:.4}')
+
